483bbd5b7dfb342b675cb9a87d88b79cc2639dc7
Stephanie974 <sturpin677@gmail.com>
2022-03-04 14:41:06 +0400

    ok

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-var xhttp = new XMLHttpRequest();
-var respJSON = [];
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    resp = this.responseText;
-    respJSON = JSON.parse(resp);
-
-    html = document.getElementById("list");
-    html.innerHTML = "";
-    debugger;
-    for (var i = 0; i < respJSON.length; i++) {
-      html.innerHTML +=
-        "<li id=" +
-        i +
-        " onclick='clickMe()'>" +
-        respJSON[i].name.official +
-        "</li> ";
-      console.log(respJSON[i]);
-    }
+async function render() {
+  // ici on cree une fonction render asynchrone qui sert a recup les données sur une meme page
+  let recuperation = await fetch(
+    // je cree une variable qui recupere à l'aide de fetch les villes en attente dans le lien
+    "https://restcountries.com/v3.1/region/europe"
+  );
+  let reponsedemoi = await recuperation.json();
+  // la variable stocke le fetch et le traduit en json car le json n'est pas comme js
+  let vue = "";
+  for (let i in reponsedemoi) {
+    vue += `<li>${reponsedemoi[i].name.official}</li>`;
+    console.log(i);
+    document.getElementById("list").innerHTML = vue;
   }
-};
-xhttp.open("GET", "https://restcountries.com/v3.1/region/europe", true);
-xhttp.send();
-function clickMe() {
-  alert("Testing");
 }
+// la boucle parcours le tableau et affiche a chaque tour un nom de plus montenegro 0, pays bas 1, france2
+render();
